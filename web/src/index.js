74a7a2f7acb572e68752519b3c5b2befb7561a76
KwakMinSeok <tonny030612@gmail.com>
2022-01-14 16:25:00 +0900

    commit
    commit

---
+++ web/src/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyCLU0dK9GNOBXOCCWw_uOXwrCEvpxdh8Mg",
+      authDomain: "myapp-b00cb.firebaseapp.com",
+      projectId: "myapp-b00cb",
+      storageBucket: "myapp-b00cb.appspot.com",
+      messagingSenderId: "183904052051",
+      appId: "1:183904052051:web:6aeaac203e5886c67937a6"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
